bfd638605ab409cbe03146f5323f10617221912d
iss <iss@>
2018-10-08 21:30:22 +0300

    improved slave roms

--- roms/make-slaves.sh
+++ roms/make-slaves.sh
@@ -6,3 +6,8 @@
 ./snap2rom.sh $1 > slave-1.rom
 cat slave-1.rom | sed -e 's/1 1983/2 1983/g' > slave-2.rom
 cat slave-1.rom | sed -e 's/1 1983/3 1983/g' > slave-3.rom
+
+# duplicate rom content for use with 27356 eproms
+cat slave-1.rom slave-1.rom > slave-1-27256.rom
+cat slave-2.rom slave-2.rom > slave-2-27256.rom
+cat slave-3.rom slave-1.rom > slave-3-27256.rom

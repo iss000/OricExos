ee0abe2df522e7f469dfeb364ebc75ab99de9f51
iss <iss@>
2019-09-09 00:25:20 +0300

    .

--- software/tests/19.bitcoin/slave.c
+++ software/tests/19.bitcoin/slave.c
@@ -2,7 +2,9 @@
 #include <oricexos.h>
 
 static const unsigned char* scrn = (unsigned char*)0xbb80;
-static const char title[] = "Bitcoin miner for OricExes";
+static const char title[] = "\x10\x07 Bitcoin miner for OricExes";
+static const char info[] =  "\x10\x02 Original code 8BITCOIN by option8";
+static const char url[] =   "\x10\x03 https://github.com/option8/8BITCOIN";
 static char* stat;
 
 void main(void)
@@ -11,9 +13,11 @@ void main(void)
   reset_exos();
   
   memset(scrn, 0x10, 40*28);
-  strcpy((char*)scrn+40, title);
-
-  stat = (char*)scrn + 40*(ipc.id*4+3);
+  strcpy((char*)scrn+1*40, title);
+  strcpy((char*)scrn+3*40, info);
+  strcpy((char*)scrn+4*40, url);
+  
+  stat = (char*)scrn + 40*(ipc.id*4+12);
 
   stat[0] = '0' + ipc.id;
 
